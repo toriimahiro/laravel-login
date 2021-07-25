a0dff1cfa583fb762f0cc2ad8c6542a95a591495
toriimahiro <muru59sgra@icloud.com>
2021-07-25 17:38:40 +0900

    create login/logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 
 class AuthController extends Controller
@@ -22,6 +24,33 @@ class AuthController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-        dd($request->all());
+        $credentials = $request->only('email','password');
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        }
+
+        return back()->withErrors([
+            'danger' => 'メールアドレスかパスワードが間違っています。'
+        ]);
+    }
+
+    /**
+     * ユーザーをアプリケーションからログアウトさせる
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('showLogin')->with('danger', 'ログアウトしました!');
     }
 }
