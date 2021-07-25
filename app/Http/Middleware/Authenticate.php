4daf6837951095b529cbc25522919d73dc8f4d85
toriimahiro <81454066+toriimahiro@users.noreply.github.com>
2021-07-25 17:51:01 +0900

    Revert "ログイン/"

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,9 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (!$request->expectsJson()) {
-            return route('login.show');
+        if (! $request->expectsJson()) {
+            return route('login');
         }
     }
-
 }
